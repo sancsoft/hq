a8e0a32da9b7cd6e6491b704efed5361514bf88a
Ryan Maffit <rmaffit@sancsoft.com>
2023-09-17 15:25:39 -0400

    Added new server platform projects

---
+++ platform/server/HQ.Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HQ.Auth.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ platform/server/HQ/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace HQ;
+
+public class Class1
+{
+
+}
