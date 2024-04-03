b7085666542878d40af1e33073e99d6b26dd6c18
Ryan Maffit <rmaffit@sancsoft.com>
2024-04-03 18:50:25 -0400

    Added Spectre.Console.Cli, moved startup to API command.

---
+++ src/dotnet/HQ/Commands/APICommand.cs
@@ -0,0 +1,35 @@
+﻿using Spectre.Console.Cli;
+
+namespace HQ.Commands;
+
+public class APICommand : AsyncCommand
+{
+    public override Task<int> ExecuteAsync(CommandContext context)
+    {
+        var args = context.Remaining.Raw.ToArray();
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+
+        return Task.FromResult(0);
+    }
+}
--- src/dotnet/HQ/Program.cs
+++ src/dotnet/HQ/Program.cs
@@ -1,22 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using HQ.Commands;
+using Spectre.Console.Cli;
 
-// Add services to the container.
+var app = new CommandApp();
+app.Configure(config =>
+{
+    config.AddCommand<APICommand>("api")
+        .WithDescription("Start HQ API");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+});
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+return await app.RunAsync(args);
