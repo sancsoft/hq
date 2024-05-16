a8d37c250582a4733140a68a46761f9ea94ed67d
amrmahdyy <amrmahdy2@gmail.com>
2024-05-15 21:45:20 -0400

    made client as an observable

--- src/angular/hq/src/app/clients/client-details/client-details-summary/client-details-summary.component.ts
+++ src/angular/hq/src/app/clients/client-details/client-details-summary/client-details-summary.component.ts
@@ -1,27 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
-import { firstValueFrom, switchMap, of, Observable, catchError, map } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { switchMap, catchError, map } from 'rxjs/operators';
 import { HQService } from '../../../services/hq.service';
 import { GetClientRecordV1 } from '../../../models/clients/get-client-v1';
 import { APIError } from '../../../errors/apierror';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'hq-client-details-summary',
   standalone: true,
-  imports: [],
-  templateUrl: './client-details-summary.component.html'
+  imports: [CommonModule],
+  templateUrl: './client-details-summary.component.html',
 })
 export class ClientDetailsSummaryComponent implements OnInit {
-    clientId?: string;
-    client?: GetClientRecordV1;
-    apiErrors: string[] = [];
+  clientId?: string;
+  client?: Observable<GetClientRecordV1 | null>;
+  apiErrors: string[] = [];
 
-    async ngOnInit(): Promise<void> {
-      this.route.paramMap.pipe(
-        switchMap(params => {
-          this.clientId = params.get('clientId') || undefined;
-          return this.getClient().pipe(
-            catchError(error => {
+  constructor(private hqService: HQService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.client = this.route.paramMap.pipe(
+      switchMap((params) => {
+        this.clientId = params.get('clientId') || undefined;
+        if (this.clientId) {
+          const request = { id: this.clientId };
+          return this.hqService.getClientsV1(request).pipe(
+            map((response) => response.records[0] || null),
+            catchError((error) => {
               if (error instanceof APIError) {
                 this.apiErrors = error.errors;
               } else {
@@ -30,21 +38,10 @@ export class ClientDetailsSummaryComponent implements OnInit {
               return of(null);
             })
           );
-        })
-      ).subscribe(client => {
-        this.client = client ?? undefined;
-      });
-    }
-    constructor(private hqService: HQService, private router: Router, private route: ActivatedRoute) { }
-
-
-    private getClient(): Observable<GetClientRecordV1> {
-      const request = { "id": this.clientId };
-      return this.hqService.getClientsV1(request).pipe(
-        map(response => response.records[0]),
-        catchError(error => {
-          throw error;
-        })
-      );
-    }
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
 }
