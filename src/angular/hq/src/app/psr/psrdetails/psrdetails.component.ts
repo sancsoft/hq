3acb7abc9455274f946da7cfd623783da9289086
amrmahdy <amahdy@sancsoft.com>
2024-05-22 16:36:46 -0400

    Added search, filter and fixed shift functionality bug

--- src/angular/hq/src/app/psr/psrdetails/psrdetails.component.ts
+++ src/angular/hq/src/app/psr/psrdetails/psrdetails.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener } from '@angular/core';
+import { PsrDetailsHeaderComponent } from './../psr-details-header/psr-details-header.component';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { SortDirection } from '../../models/common/sort-direction';
 import {
   GetPSRTimeRecordV1,
@@ -14,17 +15,27 @@ import {
   map,
   merge,
   shareReplay,
+  startWith,
   switchMap,
+  tap,
 } from 'rxjs';
 import { HQService } from '../../services/hq.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TimeStatus } from '../../models/common/time-status';
+import { PsrService } from '../psr-service';
+import { SortIconComponent } from '../../common/sort-icon/sort-icon.component';
+import { PsrDetailsSearchFilterComponent } from '../psr-details-search-filter/psr-details-search-filter.component';
 
 @Component({
   selector: 'hq-psrdetails',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    PsrDetailsHeaderComponent,
+    SortIconComponent,
+    PsrDetailsSearchFilterComponent,
+  ],
   templateUrl: './psrdetails.component.html',
 })
 export class PSRDetailsComponent {
@@ -46,20 +57,24 @@ export class PSRDetailsComponent {
   sortDirection = SortDirection;
   timeStatus = TimeStatus;
 
-  constructor(private hqService: HQService, private route: ActivatedRoute) {
+
+  constructor(
+    private hqService: HQService,
+    private route: ActivatedRoute,
+    private psrService: PsrService
+  ) {
     this.sortOption$ = new BehaviorSubject<SortColumn>(SortColumn.Date);
     this.sortDirection$ = new BehaviorSubject<SortDirection>(SortDirection.Asc);
 
-    // const search$ = psrService.search.valueChanges.pipe(
-    //   tap((t) => this.goToPage(1)),
-    //   startWith(psrService.search.value)
-    // );
+    const search$ = psrService.search.valueChanges.pipe(
+      startWith(psrService.search.value)
+    );
 
     const psrId$ = this.route.params.pipe(map((params) => params['psrId']));
     this.psrId$ = psrId$;
 
     const request$ = combineLatest({
-      // search: search$,
+      search: search$,
       projectStatusReportId: psrId$,
       sortBy: this.sortOption$,
       sortDirection: this.sortDirection$,
@@ -70,22 +85,23 @@ export class PSRDetailsComponent {
       switchMap((request) => this.hqService.getPSRTimeV1(request))
     );
 
-    const refresh$ = this.refresh$.pipe(
-      switchMap(() => apiResponse$)
-    );
+    const refresh$ = this.refresh$.pipe(switchMap(() => apiResponse$));
 
-    const response$ = merge(apiResponse$, refresh$).pipe(
-      shareReplay(1)
-    );
+    const response$ = merge(apiResponse$, refresh$).pipe(shareReplay(1));
 
     this.time$ = response$.pipe(
       map((response) => {
         return response.records;
       })
     );
-    this.timeIds$ = this.time$.pipe(map((response) => {
-      return response.map((t) => t.id);
-    }))
+    console.log(1);
+    this.time$.subscribe((t) => console.log(t));
+    this.timeIds$ = this.time$.pipe(
+      map((response) => {
+        return response.map((t) => t.id);
+      })
+    );
+    psrService.hideProjectStatus();
   }
 
   @HostListener('window:keydown', ['$event'])
@@ -127,16 +143,24 @@ export class PSRDetailsComponent {
     }
 
     if (shift) {
-      let startRowTimeIndex = 0
-      let endRowTimeIndex = 0
+      let startRowTimeIndex = 0;
+      let endRowTimeIndex = 0;
       let timeIds = await firstValueFrom(this.timeIds$);
       let indexForShiftTime = timeIds.indexOf(timeId);
       let indexesOfSelectedTimes = selected.map((t) => timeIds.indexOf(t));
-      startRowTimeIndex = Math.min(Math.min(...indexesOfSelectedTimes), indexForShiftTime);
-      endRowTimeIndex = Math.max(Math.max(...indexesOfSelectedTimes), indexForShiftTime);
+      startRowTimeIndex = Math.min(
+        Math.min(...indexesOfSelectedTimes),
+        indexForShiftTime
+      );
+      endRowTimeIndex = Math.max(
+        Math.max(...indexesOfSelectedTimes),
+        indexForShiftTime
+      );
 
       this.deselectAll();
       selected = timeIds.slice(startRowTimeIndex, endRowTimeIndex + 1);
+      const pendingTimes = await firstValueFrom(this.time$.pipe(map((t) => t.filter((t) => t.status == TimeStatus.Pending).map((t) => t.id))));
+      selected = selected.filter((t) => pendingTimes.includes(t));
     }
 
     this.selectedTimes$.next(selected);
@@ -146,14 +170,16 @@ export class PSRDetailsComponent {
   async accept(timeIds: string[]) {
     const psrId = await firstValueFrom(this.psrId$);
 
-    if(timeIds.length == 0) {
+    if (timeIds.length == 0) {
       return;
     }
 
-    const response = await firstValueFrom(this.hqService.approvePSRTimeV1({
-      projectStatusReportId: psrId,
-      timeIds: timeIds
-    }));
+    const response = await firstValueFrom(
+      this.hqService.approvePSRTimeV1({
+        projectStatusReportId: psrId,
+        timeIds: timeIds,
+      })
+    );
 
     this.refresh$.next();
     this.deselectAll();
@@ -165,7 +191,9 @@ export class PSRDetailsComponent {
   }
 
   async acceptAll() {
-    const allTime = await firstValueFrom(this.time$.pipe(map(t => t.map(x => x.id))));
+    const allTime = await firstValueFrom(
+      this.time$.pipe(map((t) => t.map((x) => x.id)))
+    );
     await this.accept(allTime);
   }
 
@@ -176,9 +204,11 @@ export class PSRDetailsComponent {
   async updateDescription(timeId: string, event: Event) {
     const description = (event.target as HTMLInputElement).value;
     const psrId = await firstValueFrom(this.psrId$);
-    const time = await firstValueFrom(this.time$.pipe(map(times => times.find(x => x.id == timeId))));
-    
-    if(!time) {
+    const time = await firstValueFrom(
+      this.time$.pipe(map((times) => times.find((x) => x.id == timeId)))
+    );
+
+    if (!time) {
       // TODO: Alert the users
       return;
     }
@@ -188,7 +218,7 @@ export class PSRDetailsComponent {
       timeId: timeId,
       billableHours: time.billableHours,
       activity: time.activity,
-      notes: description
+      notes: description,
     };
 
     // TOOD: Call API
@@ -198,25 +228,66 @@ export class PSRDetailsComponent {
 
   async updateBillableHours(timeId: string, event: Event) {
     const billableHours = (event.target as HTMLInputElement).value;
+    const roundedBillableHours = this.roundToNextQuarter(billableHours);
+    console.log(roundedBillableHours);
     const psrId = await firstValueFrom(this.psrId$);
-    const time = await firstValueFrom(this.time$.pipe(map(times => times.find(x => x.id == timeId))));
-    
-    if(!time) {
-      // TODO: Alert the users
+    const time = await firstValueFrom(
+      this.time$.pipe(map((times) => times.find((x) => x.id == timeId)))
+    );
+
+    if (!time || billableHours == '0' || billableHours == '') {
+      alert("Please Add a time to your billable hours")
       return;
     }
 
     const request = {
       projectStatusReportId: psrId,
       timeId: timeId,
-      billableHours: billableHours,
+      billableHours: roundedBillableHours,
       activity: time.activity,
-      notes: time.description
+      notes: time.description,
     };
+    //  Call API
+    this.refresh$.next();
 
-    // TOOD: Call API
 
     console.log('Call update API', request);
   }
 
+  // Reject
+  async reject(timeId: string) {
+    const psrId = await firstValueFrom(this.psrId$);
+    console.log(timeId);
+
+    if (timeId === '') {
+      return;
+    }
+
+    const response = await firstValueFrom(
+      this.hqService.rejectPSRTimeV1({
+        projectStatusReportId: psrId,
+        timeId: timeId,
+      })
+    );
+    console.log(response);
+
+    this.refresh$.next();
+    this.deselectAll();
+  }
+
+  onSortClick(sortColumn: SortColumn) {
+    if (this.sortOption$.value == sortColumn) {
+      this.sortDirection$.next(
+        this.sortDirection$.value == SortDirection.Asc
+          ? SortDirection.Desc
+          : SortDirection.Asc
+      );
+    } else {
+      this.sortOption$.next(sortColumn);
+      this.sortDirection$.next(SortDirection.Asc);
+    }
+  }
+  roundToNextQuarter(num: string | number) {
+    return Math.ceil(Number(num) * 4) / 4;
+  }
 }
