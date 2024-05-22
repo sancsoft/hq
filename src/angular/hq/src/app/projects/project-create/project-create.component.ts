fb2606548845c0e5b1fc9389b2f0a891c65c551f
ncross <ncross@sancsoft.com>
2024-05-22 09:57:47 -0400

    added ability to open/close modal when clicking on the client select option.

--- src/angular/hq/src/app/projects/project-create/project-create.component.ts
+++ src/angular/hq/src/app/projects/project-create/project-create.component.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'hq-project-create',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-create.component.html'
 })
 export class ProjectCreateComponent {
-
+  modalOpen = false;
 }
