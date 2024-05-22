6fdb5e18e9462eab55a31fae90f24b0d98607350
amrmahdy <amahdy@sancsoft.com>
2024-05-22 10:36:07 -0400

    Added shift feature

--- src/angular/hq/src/app/psr/psrdetails/psrdetails.component.ts
+++ src/angular/hq/src/app/psr/psrdetails/psrdetails.component.ts
@@ -1,7 +1,19 @@
 import { Component, HostListener } from '@angular/core';
 import { SortDirection } from '../../models/common/sort-direction';
-import { GetPSRTimeRecordV1, SortColumn } from '../../models/PSR/get-psr-time-v1';
-import { BehaviorSubject, Observable, combineLatest, debounceTime, firstValueFrom, map, shareReplay, switchMap } from 'rxjs';
+import {
+  GetPSRTimeRecordV1,
+  SortColumn,
+} from '../../models/PSR/get-psr-time-v1';
+import {
+  BehaviorSubject,
+  Observable,
+  combineLatest,
+  debounceTime,
+  firstValueFrom,
+  map,
+  shareReplay,
+  switchMap,
+} from 'rxjs';
 import { HQService } from '../../services/hq.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -10,22 +22,23 @@ import { CommonModule } from '@angular/common';
   selector: 'hq-psrdetails',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './psrdetails.component.html'
+  templateUrl: './psrdetails.component.html',
 })
 export class PSRDetailsComponent {
   apiErrors: string[] = [];
-  
+
   time$: Observable<GetPSRTimeRecordV1[]>;
+  timeIds$: Observable<string[]>;
   sortOption$: BehaviorSubject<SortColumn>;
   sortDirection$: BehaviorSubject<SortDirection>;
 
   selectedTimes$ = new BehaviorSubject<string[]>([]);
-  lastSelectedTime$ = new BehaviorSubject<string|null>(null);
+  lastSelectedTime$ = new BehaviorSubject<string | null>(null);
   shiftKey$ = new BehaviorSubject<boolean>(false);
 
   sortColumn = SortColumn;
   sortDirection = SortDirection;
-  
+
   constructor(private hqService: HQService, private route: ActivatedRoute) {
     this.sortOption$ = new BehaviorSubject<SortColumn>(SortColumn.Date);
     this.sortDirection$ = new BehaviorSubject<SortDirection>(SortDirection.Asc);
@@ -35,7 +48,7 @@ export class PSRDetailsComponent {
     //   startWith(psrService.search.value)
     // );
 
-    const psrId$ = this.route.params.pipe(map(params => params['psrId']));
+    const psrId$ = this.route.params.pipe(map((params) => params['psrId']));
 
     const request$ = combineLatest({
       // search: search$,
@@ -55,20 +68,33 @@ export class PSRDetailsComponent {
         return response.records;
       })
     );
+    this.timeIds$ = this.time$.pipe(map((response) => {
+      return response.map((t) => t.id);
+    }))
+    this.timeIds$.subscribe((t) => console.log(t));
   }
 
   @HostListener('window:keydown', ['$event'])
   onKeydown(event: KeyboardEvent) {
-    this.shiftKey$.next(event.shiftKey);
+    if (event.key === 'Shift') {
+      this.shiftKey$.next(event.shiftKey);
+    }
   }
-
   @HostListener('window:keyup', ['$event'])
   onKeyup(event: KeyboardEvent) {
-    this.shiftKey$.next(event.shiftKey);
+    if (event.key === 'Shift') {
+      this.shiftKey$.next(event.shiftKey);
+    }
+  }
+  @HostListener('window:blur', ['$event'])
+  onBlur(event: KeyboardEvent) {
+    this.shiftKey$.next(false);
   }
 
   isSelected(timeId: string) {
-    return this.selectedTimes$.pipe(map(selected => selected.includes(timeId)));
+    return this.selectedTimes$.pipe(
+      map((selected) => selected.includes(timeId))
+    );
   }
 
   deselectAll() {
@@ -77,22 +103,29 @@ export class PSRDetailsComponent {
   }
 
   async toggleTime(timeId: string) {
-    let selected = [...await firstValueFrom(this.selectedTimes$)];
+    let selected = [...(await firstValueFrom(this.selectedTimes$))];
     let shift = await firstValueFrom(this.shiftKey$);
-    
+
     if (selected.includes(timeId)) {
       selected.splice(selected.indexOf(timeId), 1);
     } else {
       selected.push(timeId);
     }
 
-    if(shift) {
-      alert('Holding shift');
-      // select indexes from lastSelcetedTimed to index of timeId
+    if (shift) {
+      let startRowTimeIndex = 0
+      let endRowTimeIndex = 0
+      let timeIds = await firstValueFrom(this.timeIds$);
+      let indexForShiftTime = timeIds.indexOf(timeId);
+      let indexesOfSelectedTimes = selected.map((t) => timeIds.indexOf(t));
+      startRowTimeIndex = Math.min(Math.min(...indexesOfSelectedTimes), indexForShiftTime);
+      endRowTimeIndex = Math.max(Math.max(...indexesOfSelectedTimes), indexForShiftTime);
+
+      this.deselectAll();
+      selected = timeIds.slice(startRowTimeIndex, endRowTimeIndex + 1);
     }
 
     this.selectedTimes$.next(selected);
     this.lastSelectedTime$.next(timeId);
   }
-
 }
