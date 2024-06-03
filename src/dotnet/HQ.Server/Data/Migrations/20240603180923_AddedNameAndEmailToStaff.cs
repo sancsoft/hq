896f3cc39fdf7f3c0fba649df522a179fb3b791b
Ryan Maffit <rmaffit@sancsoft.com>
2024-06-03 18:10:05 +0000

    Added migration to add name and emails to staff

---
+++ src/dotnet/HQ.Server/Data/Migrations/20240603180923_AddedNameAndEmailToStaff.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HQ.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNameAndEmailToStaff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_times_project_activity_activity_id",
+                table: "times");
+
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "staff",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "first_name",
+                table: "staff",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "last_name",
+                table: "staff",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_times_project_activities_activity_id",
+                table: "times",
+                column: "activity_id",
+                principalTable: "project_activities",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_times_project_activities_activity_id",
+                table: "times");
+
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "staff");
+
+            migrationBuilder.DropColumn(
+                name: "first_name",
+                table: "staff");
+
+            migrationBuilder.DropColumn(
+                name: "last_name",
+                table: "staff");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_times_project_activity_activity_id",
+                table: "times",
+                column: "activity_id",
+                principalTable: "project_activities",
+                principalColumn: "id");
+        }
+    }
+}
--- src/dotnet/HQ.Server/Data/Models/Staff.cs
+++ src/dotnet/HQ.Server/Data/Models/Staff.cs
@@ -4,10 +4,10 @@ namespace HQ.Server.Data.Models;
 
 public class Staff : Base
 {
+    public string Name { get; set; } = null!;
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Email { get; set; }
-    public string Name { get; set; } = null!;
     public int WorkHours { get; set; }
     public int VacationHours { get; set; }
     public Jurisdiciton Jurisdiciton { get; set; }
