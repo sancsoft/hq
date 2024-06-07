31bf09212e3539fdce2b5c783f82df7fe895048f
pshahsancsoft <pshah@sancsoft.com>
2024-06-07 13:26:52 -0400

    Added prompt and confirm service

--- src/angular/hq/src/app/services/modal.service.ts
+++ src/angular/hq/src/app/services/modal.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject, map, of } from 'rxjs';
 import { Dialog } from '@angular/cdk/dialog';
 import { AlertModalComponent } from '../common/alert-modal/alert-modal.component';
+import { ConfirmModalComponent } from '../common/confirm-modal/confirm-modal.component';
+import { PromptModalComponent } from '../common/prompt-modal/prompt-modal.component';
 
 export interface ModalData {
   title: string;
@@ -16,7 +18,14 @@ export class ModalService {
   constructor(public dialog: Dialog) { }
 
   confirm(title: string, message: string = ''): Observable<boolean> {
-    return of(window.confirm(`${title}\n${message}`));
+    const dialogRef = this.dialog.open<boolean, ModalData>(ConfirmModalComponent,{
+      minWidth: '600px',
+      data:{
+        title,
+        message,
+      }
+    });
+    return dialogRef.closed.pipe(map(t=>t === true ));
   }
 
   alert(title: string, message: string): Observable<boolean> {
@@ -31,12 +40,19 @@ export class ModalService {
     return dialogRef.closed.pipe(map(t => true));
   }
 
-  prompt(title: string, message: string = ''): Observable<string|null> {
-    return of(window.prompt(`${title}\n${message}`));
+  prompt(title: string, message: string = ''): Observable<string|undefined> {
+    const dialogRef = this.dialog.open<string, ModalData>(PromptModalComponent, {
+      minWidth: '600px',
+      data: {
+        title,
+        message,
+      }
+    });
+    return dialogRef.closed.pipe(map(t=>t));
   }
 
   // showCustom(title: string, component: ComponentType<C>, data: ?): Observable<?> {
-    
+
   // }
 
 }
