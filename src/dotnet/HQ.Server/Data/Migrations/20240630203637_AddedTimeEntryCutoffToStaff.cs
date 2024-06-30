c7bb14fecfa783b95dd61ab0bf3b71130c3be3c7
Ryan Maffit <rmaffit@sancsoft.com>
2024-06-30 20:39:40 +0000

    Added `TimeEntryCutoffDate` to staff entity

---
+++ src/dotnet/HQ.Server/Data/Migrations/20240630203637_AddedTimeEntryCutoffToStaff.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HQ.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTimeEntryCutoffToStaff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "time_entry_cutoff_date",
+                table: "staff",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "time_entry_cutoff_date",
+                table: "staff");
+        }
+    }
+}
