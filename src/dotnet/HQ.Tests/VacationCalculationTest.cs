0023682f335023195ff14afcb8b8c4c8ec1f9a55
Ryan Maffit <rmaffit@sancsoft.com>
2024-07-01 16:22:52 +0000

    Updated vacation calculation rules

--- src/dotnet/HQ.Tests/VacationCalculationTest.cs
+++ src/dotnet/HQ.Tests/VacationCalculationTest.cs
@@ -10,8 +10,8 @@ public class VacationCalculationTest
     [InlineData(120, 0)]
     public void Staff_GetsNoVacationDaysFirstMonth(decimal vacationHours, decimal expectedVacationHours)
     {
-        var startDate = new DateOnly(2024, 06, 20);
-        var date = new DateOnly(2024, 06, 25);
+        var startDate = new DateOnly(2024, 6, 20);
+        var date = new DateOnly(2024, 6, 25);
 
         var calculatedVacationHours = date.CalculateEarnedVacationHours(startDate, vacationHours);
 
@@ -22,10 +22,10 @@ public class VacationCalculationTest
     [InlineData(0, 0)]
     [InlineData(80, 8)]
     [InlineData(120, 8)]
-    public void Staff_GetsOneVacationDayNextMonth(decimal vacationHours, decimal expectedVacationHours)
+    public void Staff_GetsOneVacationAfterOneFullMonth(decimal vacationHours, decimal expectedVacationHours)
     {
         var startDate = new DateOnly(2024, 6, 20);
-        var date = new DateOnly(2024, 7, 1);
+        var date = new DateOnly(2024, 8, 1);
 
         var calculatedVacationHours = date.CalculateEarnedVacationHours(startDate, vacationHours);
 
@@ -36,10 +36,24 @@ public class VacationCalculationTest
     [InlineData(0, 0)]
     [InlineData(80, 16)]
     [InlineData(120, 16)]
-    public void Staff_GetsTwoVacationDaysAfterTwoMonths(decimal vacationHours, decimal expectedVacationHours)
+    public void Staff_GetsTwoVacationDaysAfterTwoFullMonths(decimal vacationHours, decimal expectedVacationHours)
     {
         var startDate = new DateOnly(2024, 6, 20);
-        var date = new DateOnly(2024, 8, 1);
+        var date = new DateOnly(2024, 9, 1);
+
+        var calculatedVacationHours = date.CalculateEarnedVacationHours(startDate, vacationHours);
+
+        Assert.Equal(expectedVacationHours, calculatedVacationHours);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(80, 80)]
+    [InlineData(120, 120)]
+    public void Staff_GetsFullVacationDaysAfterNewYear(decimal vacationHours, decimal expectedVacationHours)
+    {
+        var startDate = new DateOnly(2024, 6, 20);
+        var date = new DateOnly(2025, 1, 1);
 
         var calculatedVacationHours = date.CalculateEarnedVacationHours(startDate, vacationHours);
 
