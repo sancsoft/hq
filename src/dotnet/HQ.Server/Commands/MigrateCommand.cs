db76ead95d3105461f2484ea68d9ac40c2fdadce
amahdysancsoft <amahdy@sancsoft.com>
2024-07-01 21:40:04 -0400

    Added try catch in project Starter files (#193)
    * Added try catch in project Starter files
    
    * Fixed exception handling
    
    ---------
    
    Co-authored-by: Ryan Maffit <rmaffit@sancsoft.com>

--- src/dotnet/HQ.Server/Commands/MigrateCommand.cs
+++ src/dotnet/HQ.Server/Commands/MigrateCommand.cs
@@ -20,22 +20,29 @@ public class MigrateCommand : AsyncCommand
 {
     public override async Task<int> ExecuteAsync(CommandContext context)
     {
-        var args = context.Remaining.Raw.ToArray();
-        var builder = WebApplication.CreateBuilder(args);
+        try
+        {
+            var args = context.Remaining.Raw.ToArray();
+            var builder = WebApplication.CreateBuilder(args);
 
-        builder.Configuration.AddEnvironmentVariables("HQ_");
+            builder.Configuration.AddEnvironmentVariables("HQ_");
 
-        // Add services to the container.
-        builder.Services.AddHQDbContext(builder.Configuration);
+            // Add services to the container.
+            builder.Services.AddHQDbContext(builder.Configuration);
 
-        var app = builder.Build();
+            var app = builder.Build();
 
-        var serviceScopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
-        await using var scope = serviceScopeFactory.CreateAsyncScope();
-        await using var dbContext = scope.ServiceProvider.GetRequiredService<HQDbContext>();
+            var serviceScopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+            await using var scope = serviceScopeFactory.CreateAsyncScope();
+            await using var dbContext = scope.ServiceProvider.GetRequiredService<HQDbContext>();
 
-        await dbContext.Database.MigrateAsync();
+            await dbContext.Database.MigrateAsync();
 
-        return 0;
+            return 0;
+        }
+        catch (Exception ex) when (ex is HostAbortedException && ex.Source == "Microsoft.EntityFrameworkCore.Design") // see https://github.com/dotnet/efcore/issues/29923
+        {
+            return 0;
+        }
     }
 }
