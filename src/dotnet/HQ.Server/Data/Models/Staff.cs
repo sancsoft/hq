bca81cdf8fa3c86e4a4bf7d4c958dfc49525d837
Ryan Maffit <rmaffit@sancsoft.com>
2024-07-02 23:06:23 -0400

    Update Staff.cs

--- src/dotnet/HQ.Server/Data/Models/Staff.cs
+++ src/dotnet/HQ.Server/Data/Models/Staff.cs
@@ -7,7 +7,7 @@ public class Staff : Base
     public string Name { get; set; } = null!;
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    public string Email { get; set; } = null!;
+    public string? Email { get; set; }
     public int WorkHours { get; set; }
     public int VacationHours { get; set; }
     public Jurisdiciton Jurisdiciton { get; set; }
