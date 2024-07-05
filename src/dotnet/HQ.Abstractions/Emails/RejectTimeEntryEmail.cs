072db2c0b4052fde3b7bb7a6d33601ef8e37ee5d
Ryan Maffit <rmaffit@sancsoft.com>
2024-07-05 01:36:39 +0000

    Model cleanup

--- src/dotnet/HQ.Abstractions/Emails/RejectTimeEntryEmail.cs
+++ src/dotnet/HQ.Abstractions/Emails/RejectTimeEntryEmail.cs
@@ -4,7 +4,6 @@ namespace HQ.Abstractions.Emails
     {
         public string? Date { get; set; }
         public string? ChargeCode { get; set; }
-        public string? TimeEntryId { get; set; }
         public string? Client { get; set; }
         public string? Project { get; set; }
         public string? ActivityTask { get; set; }
@@ -12,8 +11,8 @@ namespace HQ.Abstractions.Emails
         public string? ReasonForRejection { get; set; }
         public static new RejectTimeEntryEmail Sample = new()
         {
-            Heading = "[HQ] Time Rejected",
-            Message = "Your time entry has been rejected. Please review the details and resubmit.",
+            Heading = "Time Rejected",
+            Message = "Your time entry has been rejected. Please review and resubmit.",
             ButtonLabel = "Open HQ",
             ButtonUrl = new Uri("http://hq.localhost:4200/dashboard"),
             Date = "07/03/2024",
