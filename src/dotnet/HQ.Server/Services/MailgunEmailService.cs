cead8e11ff545d0c1fa073e3408813190d5a1d2d
Ryan Maffit <rmaffit@sancsoft.com>
2024-07-05 02:39:04 +0000

    Added debug mode support to mailgun service

--- src/dotnet/HQ.Server/Services/MailgunEmailService.cs
+++ src/dotnet/HQ.Server/Services/MailgunEmailService.cs
@@ -7,6 +7,8 @@ using HQ.Server.Data.Models;
 
 using Microsoft.Extensions.Options;
 
+using MimeKit;
+
 public class MailgunEmailService : IEmailService
 {
     private readonly HttpClient _httpClient;
@@ -25,16 +27,65 @@ public class MailgunEmailService : IEmailService
 
     public async Task SendAsync(string subject, string htmlBody, string textBody, List<string> recipients, IEnumerable<Attachment>? attachments = null, MailPriority mailPriority = MailPriority.Normal, CancellationToken ct = default)
     {
-        var recipientsJoinedWithCommas = String.Join(",", recipients);
-        var formContent = new FormUrlEncodedContent(new[]
-        {
-            new KeyValuePair<string, string>("from", $"{_options.CurrentValue.FromDisplayName} <{_options.CurrentValue.From}>"),
-            new KeyValuePair<string, string>("to", recipientsJoinedWithCommas),
-            new KeyValuePair<string, string>("subject", subject),
-            new KeyValuePair<string, string>("html", htmlBody)
-        });
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress(_options.CurrentValue.FromDisplayName, _options.CurrentValue.From));
+        message.To.AddRange(recipients.Select(t => new MailboxAddress(null, t)));
+        message.Subject = subject;
+        message.Priority = (MessagePriority)mailPriority;
 
-        var response = await _httpClient.PostAsync($"{_options.CurrentValue.BaseUri}/{_options.CurrentValue.Domain}/messages", formContent, ct);
+        var builder = new BodyBuilder();
+        builder.TextBody = textBody;
+        builder.HtmlBody = htmlBody;
+
+        if (attachments != null)
+        {
+            foreach (var attachment in attachments)
+            {
+                builder.Attachments.Add(attachment.Name, attachment.ContentStream, ct);
+            }
+        }
+
+        message.Body = builder.ToMessageBody();
+
+        if (_options.CurrentValue.IsDebug)
+        {
+            if (!_options.CurrentValue.DebugAddress.Any())
+            {
+                _logger.LogWarning("No DebugAddress emails found.");
+                return;
+            }
+
+            MimeMessage debugMessage = new();
+            debugMessage.From.Add(new MailboxAddress(_options.CurrentValue.FromDisplayName, _options.CurrentValue.From));
+            debugMessage.To.AddRange(_options.CurrentValue.DebugAddress.Select(t => new MailboxAddress(Encoding.UTF8, null, t)));
+            debugMessage.Subject = String.Format("[DEBUG MODE] - {0}", message.Subject);
+            debugMessage.Priority = message.Priority;
+
+            builder = new();
+
+            byte[] rawMessage = Encoding.UTF8.GetBytes(message.ToString());
+
+            builder.Attachments.Add(message.Subject + ".eml", rawMessage);
+            builder.TextBody = "This is a debug message, the original email is attached.";
+
+            debugMessage.Body = builder.ToMessageBody();
+
+            message = debugMessage;
+        }
+
+        using var messageStream = new MemoryStream(Encoding.UTF8.GetBytes(message.ToString()));
+        var to = String.Join(",", message.To);
+
+        MultipartFormDataContent postData = new MultipartFormDataContent();
+        postData.Add(new StringContent(to), "to");
+        postData.Add(new StreamContent(messageStream), "message", "message.eml");
+
+        var response = await _httpClient.PostAsync($"{_options.CurrentValue.BaseUri}/{_options.CurrentValue.Domain}/messages.mime", postData, ct);
+        var responseText = await response.Content.ReadAsStringAsync(ct);
+        if (!response.IsSuccessStatusCode)
+        {
+            _logger.LogWarning("Mailgun Response: {Response}", responseText);
+        }
 
         response.EnsureSuccessStatusCode();
     }
@@ -51,5 +102,8 @@ public class MailgunEmailService : IEmailService
         public string From { get; set; } = null!;
         [Required]
         public string FromDisplayName { get; set; } = null!;
+
+        public bool IsDebug { get; set; }
+        public List<string> DebugAddress { get; set; } = new();
     }
 }
