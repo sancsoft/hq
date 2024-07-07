f2954ad2e6d4f01d05aa562097eee49a665a1e95
Ryan Maffit <rmaffit@sancsoft.com>
2024-07-07 19:08:02 +0000

    Reworked notification email into layout for reusability

--- src/dotnet/HQ.Abstractions/Emails/RejectTimeEntryEmail.cs
+++ src/dotnet/HQ.Abstractions/Emails/RejectTimeEntryEmail.cs
@@ -2,26 +2,30 @@ namespace HQ.Abstractions.Emails
 {
     public class RejectTimeEntryEmail : NotificationEmail
     {
-        public string? Date { get; set; }
-        public string? ChargeCode { get; set; }
-        public string? Client { get; set; }
-        public string? Project { get; set; }
+        public string Date { get; set; } = null!;
+        public decimal Hours { get; set; }
+        public string ChargeCode { get; set; } = null!;
+        public string Client { get; set; } = null!;
+        public string Project { get; set; } = null!;
         public string? ActivityTask { get; set; }
         public string? Description { get; set; }
         public string? ReasonForRejection { get; set; }
+        public string? RejectedBy { get; set; }
         public static new RejectTimeEntryEmail Sample = new()
         {
             Heading = "Time Rejected",
             Message = "Your time entry has been rejected. Please review and resubmit.",
+            Hours = 0,
             ButtonLabel = "Open HQ",
             ButtonUrl = new Uri("http://hq.localhost:4200/dashboard"),
             Date = "07/03/2024",
             ChargeCode = "P1041",
             Client = "SANCSOFT",
             Project = "HQ",
-            ActivityTask = "qqq",
-            Description = "AQAA",
-            ReasonForRejection = "The hours entered (0) are not valid. Please ensure that the hours reflect actual time worked."
+            ActivityTask = "#123",
+            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            ReasonForRejection = "The hours entered (0) are not valid. Please ensure that the hours reflect actual time worked.",
+            RejectedBy = "Joe Fabitz"
         };
     }
 
