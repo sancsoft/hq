b8c55d3f1cc3f48765625f5e7da71cfba709b40d
Ryan Maffit <rmaffit@sancsoft.com>
2024-07-09 15:49:24 +0000

    WIP Search

--- src/angular/hq/src/app/core/components/search-input/search-input.component.ts
+++ src/angular/hq/src/app/core/components/search-input/search-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'hq-search-input',
@@ -6,8 +7,43 @@ import { Component, ElementRef, Input, ViewChild } from '@angular/core';
   imports: [],
   templateUrl: './search-input.component.html',
 })
-export class SearchInputComponent {
+export class SearchInputComponent implements ControlValueAccessor {
+  constructor(public ngControl: NgControl) {
+    ngControl.valueAccessor = this;
+  }
+
   protected focused = false;
+  protected disabled = false;
+
+  private _value: string | null = null;
+
+  set value(value: string | null) {
+    this._value = value;
+    this.onChange(value);
+  }
+
+  get value(): string | null {
+    return this._value;
+  }
+
+  onChange: (value: string | null) => void = () => {};
+  onTouched: (value: string | null) => void = () => {};
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(onChange: (value: string | null) => void) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: (value: string | null) => void) {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 
   @ViewChild('input')
   input?: ElementRef<HTMLInputElement>;
