6e6143af5ddd4cff6166414e6384296882108ccd
amahdysancsoft <amahdy@sancsoft.com>
2024-07-11 15:59:42 -0400

    Progress bar reusable component ceated

--- src/angular/hq/src/app/core/components/progress-bar/progress-bar.component.ts
+++ src/angular/hq/src/app/core/components/progress-bar/progress-bar.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProjectStatus } from '../../../clients/client-details.service';
+import { Period } from '../../../models/times/get-time-v1';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'hq-progress-bar',
   standalone: true,
-  imports: [],
-  templateUrl: './progress-bar.component.html'
+  imports: [CommonModule],
+  templateUrl: './progress-bar.component.html',
 })
 export class ProgressBarComponent {
+  @Input()
+  tooltip: string = '';
+  @Input()
+  text: string | null = '';
+  @Input()
+  percent: number | null | undefined = 0;
 
+  Math = Math;
 }
