cb3bde7b988adadebb1ff31c3dff4f541359e093
Ryan Maffit <rmaffit@sancsoft.com>
2024-07-16 03:12:53 +0000

    Updated client details search with new input components

--- src/angular/hq/src/app/clients/client-details/client-details-search-filter/client-details-search-filter.component.ts
+++ src/angular/hq/src/app/clients/client-details/client-details-search-filter/client-details-search-filter.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ClientDetailsService } from '../client-details.service';
+import { SearchInputComponent } from '../../../core/components/search-input/search-input.component';
+import { SelectInputComponent } from '../../../core/components/select-input/select-input.component';
+import { SelectInputOptionDirective } from '../../../core/directives/select-input-option.directive';
 
 @Component({
   selector: 'hq-client-details-search-filter',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SearchInputComponent,
+    SelectInputComponent,
+    SelectInputOptionDirective,
+  ],
   templateUrl: './client-details-search-filter.component.html',
 })
 export class ClientDetailsSearchFilterComponent {
