7aeb783a1cd9c00c2283a9cdc31243daac987363
Ryan Maffit <rmaffit@sancsoft.com>
2024-07-16 03:16:23 +0000

    Updated client details summary to use new stats display component

--- src/angular/hq/src/app/clients/client-details/client-details-summary/client-details-summary.component.ts
+++ src/angular/hq/src/app/clients/client-details/client-details-summary/client-details-summary.component.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ErrorDisplayComponent } from '../../../errors/error-display/error-display.component';
 import { RouterLink } from '@angular/router';
 import { ClientDetailsService } from '../client-details.service';
+import { StatDisplayComponent } from '../../../core/components/stat-display/stat-display.component';
 @Component({
   selector: 'hq-client-details-summary',
   standalone: true,
-  imports: [CommonModule, ErrorDisplayComponent, RouterLink],
+  imports: [
+    CommonModule,
+    ErrorDisplayComponent,
+    RouterLink,
+    StatDisplayComponent,
+  ],
   templateUrl: './client-details-summary.component.html',
 })
 export class ClientDetailsSummaryComponent {
