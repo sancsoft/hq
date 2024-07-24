c4d5086e6cd87aa0b9c7250335837fcce1b02714
Ryan Maffit <rmaffit@sancsoft.com>
2024-07-24 22:00:14 +0000

    Quote list cleanup

--- src/angular/hq/src/app/quotes/quotes-list/quote-list.service.ts
+++ src/angular/hq/src/app/quotes/quotes-list/quote-list.service.ts
@@ -1,9 +1,115 @@
 import { Injectable } from '@angular/core';
+import { ProjectStatus } from '../../enums/project-status';
+import { FormControl } from '@angular/forms';
+import {
+  BehaviorSubject,
+  combineLatest,
+  debounceTime,
+  map,
+  merge,
+  Observable,
+  shareReplay,
+  Subject,
+  switchMap,
+  tap,
+} from 'rxjs';
+import { HQService } from '../../services/hq.service';
+import { SortDirection } from '../../models/common/sort-direction';
+import {
+  GetQuotesRecordV1,
+  SortColumn,
+} from '../../models/quotes/get-quotes-v1';
+import { formControlChanges } from '../../core/functions/form-control-changes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuoteListService {
+  public ProjectStatus = ProjectStatus;
 
-  constructor() { }
+  search = new FormControl<string | null>(null);
+  quoteStatus = new FormControl<ProjectStatus | null>(null);
+
+  quotes$: Observable<GetQuotesRecordV1[]>;
+  skipDisplay$: Observable<number>;
+  takeToDisplay$: Observable<number>;
+  totalRecords$: Observable<number>;
+  sortOption$: BehaviorSubject<SortColumn>;
+  sortDirection$: BehaviorSubject<SortDirection>;
+
+  itemsPerPage = new FormControl(20, { nonNullable: true });
+  page = new FormControl<number>(1, { nonNullable: true });
+
+  refreshSubject = new Subject<void>();
+
+  constructor(private hqService: HQService) {
+    this.sortOption$ = new BehaviorSubject<SortColumn>(SortColumn.QuoteNumber);
+    this.sortDirection$ = new BehaviorSubject<SortDirection>(
+      SortDirection.Desc,
+    );
+
+    const itemsPerPage$ = formControlChanges(this.itemsPerPage);
+    const page$ = formControlChanges(this.page);
+
+    const skip$ = combineLatest([itemsPerPage$, page$]).pipe(
+      map(([itemsPerPage, page]) => (page - 1) * itemsPerPage),
+    );
+
+    const search$ = formControlChanges(this.search).pipe(
+      tap(() => this.goToPage(1)),
+    );
+
+    const quoteStatus$ = formControlChanges(this.quoteStatus).pipe(
+      tap(() => this.goToPage(1)),
+    );
+
+    this.skipDisplay$ = skip$.pipe(map((skip) => skip + 1));
+
+    const request$ = combineLatest({
+      search: search$,
+      skip: skip$,
+      take: itemsPerPage$,
+      sortBy: this.sortOption$,
+      quoteStatus: quoteStatus$,
+      sortDirection: this.sortDirection$,
+    }).pipe(debounceTime(500), shareReplay({ bufferSize: 1, refCount: false }));
+
+    const requestResponse$ = request$.pipe(
+      switchMap((request) => this.hqService.getQuotesV1(request)),
+    );
+
+    const refreshResponse$ = this.refreshSubject.pipe(
+      switchMap(() => requestResponse$),
+    );
+
+    const response$ = merge(requestResponse$, refreshResponse$).pipe(
+      shareReplay({ bufferSize: 1, refCount: false }),
+    );
+
+    this.quotes$ = response$.pipe(
+      map((response) => {
+        return response.records;
+      }),
+    );
+
+    this.totalRecords$ = response$.pipe(map((t) => t.total!));
+
+    this.takeToDisplay$ = combineLatest([
+      skip$,
+      itemsPerPage$,
+      this.totalRecords$,
+    ]).pipe(
+      map(([skip, itemsPerPage, totalRecords]) =>
+        Math.min(skip + itemsPerPage, totalRecords),
+      ),
+    );
+  }
+
+  goToPage(page: number) {
+    this.page.setValue(page);
+  }
+
+  refresh() {
+    this.refreshSubject.next();
+  }
 }
--- src/angular/hq/src/app/quotes/quotes-list/quotes-list.component.ts
+++ src/angular/hq/src/app/quotes/quotes-list/quotes-list.component.ts
@@ -1,27 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import {
-  Observable,
-  debounceTime,
-  switchMap,
-  combineLatest,
-  map,
-  shareReplay,
-  startWith,
-  tap,
-  BehaviorSubject,
-  firstValueFrom,
-} from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
+import { firstValueFrom } from 'rxjs';
 import { HQService } from '../../services/hq.service';
-import {
-  GetQuotesRecordV1,
-  SortColumn,
-} from '../../models/quotes/get-quotes-v1';
+import { SortColumn } from '../../models/quotes/get-quotes-v1';
 import { SortDirection } from '../../models/common/sort-direction';
 import { PaginatorComponent } from '../../common/paginator/paginator.component';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { ClientDetailsServiceToReplace } from '../../clients/client-details.service';
 import { SortIconComponent } from '../../common/sort-icon/sort-icon.component';
 import { ClientDetailsSearchFilterComponent } from '../../clients/client-details/client-details-search-filter/client-details-search-filter.component';
 import { HQRole } from '../../enums/hqrole';
@@ -29,6 +14,7 @@ import { InRolePipe } from '../../pipes/in-role.pipe';
 import { ProjectStatus } from '../../enums/project-status';
 import { CoreModule } from '../../core/core.module';
 import saveAs from 'file-saver';
+import { QuoteListService } from './quote-list.service';
 
 @Component({
   selector: 'hq-quotes-list',
@@ -48,16 +34,6 @@ import saveAs from 'file-saver';
 export class QuotesListComponent {
   apiErrors: string[] = [];
 
-  quotes$: Observable<GetQuotesRecordV1[]>;
-  skipDisplay$: Observable<number>;
-  takeToDisplay$: Observable<number>;
-  totalRecords$: Observable<number>;
-  sortOption$: BehaviorSubject<SortColumn>;
-  sortDirection$: BehaviorSubject<SortDirection>;
-
-  itemsPerPage = new FormControl(20, { nonNullable: true });
-  page = new FormControl<number>(1, { nonNullable: true });
-
   sortColumn = SortColumn;
   sortDirection = SortDirection;
   HQRole = HQRole;
@@ -65,81 +41,27 @@ export class QuotesListComponent {
   constructor(
     private hqService: HQService,
     private route: ActivatedRoute,
-    private clientDetailService: ClientDetailsServiceToReplace,
+    public quoteListService: QuoteListService,
   ) {
-    this.sortOption$ = new BehaviorSubject<SortColumn>(SortColumn.QuoteNumber);
-    this.sortDirection$ = new BehaviorSubject<SortDirection>(
-      SortDirection.Desc,
-    );
-
-    const itemsPerPage$ = this.itemsPerPage.valueChanges.pipe(
-      startWith(this.itemsPerPage.value),
-    );
-    const page$ = this.page.valueChanges.pipe(startWith(this.page.value));
-
-    const skip$ = combineLatest([itemsPerPage$, page$]).pipe(
-      map(([itemsPerPage, page]) => (page - 1) * itemsPerPage),
-      startWith(0),
-    );
-    const search$ = clientDetailService.search.valueChanges.pipe(
-      tap(() => this.goToPage(1)),
-      startWith(clientDetailService.search.value),
-    );
-
-    this.skipDisplay$ = skip$.pipe(map((skip) => skip + 1));
-
-    const request$ = combineLatest({
-      search: search$,
-      skip: skip$,
-      take: itemsPerPage$,
-      sortBy: this.sortOption$,
-      sortDirection: this.sortDirection$,
-    });
-
-    const response$ = request$.pipe(
-      debounceTime(500),
-      switchMap((request) => this.hqService.getQuotesV1(request)),
-      shareReplay({ bufferSize: 1, refCount: false }),
-    );
-
-    this.quotes$ = response$.pipe(
-      map((response) => {
-        return response.records;
-      }),
-    );
-
-    this.totalRecords$ = response$.pipe(map((t) => t.total!));
-
-    this.takeToDisplay$ = combineLatest([
-      skip$,
-      itemsPerPage$,
-      this.totalRecords$,
-    ]).pipe(
-      map(([skip, itemsPerPage, totalRecords]) =>
-        Math.min(skip + itemsPerPage, totalRecords),
-      ),
-    );
-
-    this.clientDetailService.resetFilters();
-    this.clientDetailService.hideProjectStatus();
+    this.quoteListService.refresh();
   }
 
   goToPage(page: number) {
-    this.page.setValue(page);
+    this.quoteListService.goToPage(page);
   }
 
   onSortClick(sortColumn: SortColumn) {
-    if (this.sortOption$.value == sortColumn) {
-      this.sortDirection$.next(
-        this.sortDirection$.value == SortDirection.Asc
+    if (this.quoteListService.sortOption$.value == sortColumn) {
+      this.quoteListService.sortDirection$.next(
+        this.quoteListService.sortDirection$.value == SortDirection.Asc
           ? SortDirection.Desc
           : SortDirection.Asc,
       );
     } else {
-      this.sortOption$.next(sortColumn);
-      this.sortDirection$.next(SortDirection.Asc);
+      this.quoteListService.sortOption$.next(sortColumn);
+      this.quoteListService.sortDirection$.next(SortDirection.Asc);
     }
-    this.page.setValue(1);
+    this.goToPage(1);
   }
   getProjectStatusString(status: ProjectStatus): string {
     return ProjectStatus[status];
