ef112f8347a3c4db5ccfe09fa30aed18eb320bb2
Ryan Maffit <rmaffit@sancsoft.com>
2024-08-07 22:55:29 +0000

    Fixed merge conflict

--- src/angular/hq/src/app/staff-dashboard/staff-dashboard.component.ts
+++ src/angular/hq/src/app/staff-dashboard/staff-dashboard.component.ts
@@ -270,7 +270,6 @@ export class StaffDashboardComponent implements OnInit, OnDestroy, OnChanges {
         distinctUntilChanged(),
         switchMap(() => {
           return request$.pipe(
-            skip(1),
             debounceTime(1000),
             skip(1),
             filter((t) => t.canEdit),
@@ -286,8 +285,7 @@ export class StaffDashboardComponent implements OnInit, OnDestroy, OnChanges {
       )
       // eslint-disable-next-line rxjs-angular/prefer-async-pipe,
       .subscribe({
-        next: (t) => {
-          console.log(t);
+        next: () => {
           this.toastService.show('Success', 'Plan saved successfully');
         },
         error: async () => {
