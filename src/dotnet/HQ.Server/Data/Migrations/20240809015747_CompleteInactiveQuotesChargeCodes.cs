5d87732d648e8ce35dd9171a50f605308e9f38a8
Ryan Maffit <rmaffit@sancsoft.com>
2024-08-09 01:59:41 +0000

    Mark inactive charge code projects and quotes as completed.

---
+++ src/dotnet/HQ.Server/Data/Migrations/20240809015747_CompleteInactiveQuotesChargeCodes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HQ.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompleteInactiveQuotesChargeCodes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE projects p SET status = 7 FROM charge_codes c WHERE p.id = c.project_id AND c.active = false");
+            migrationBuilder.Sql("UPDATE quotes q SET status = 7 FROM charge_codes c WHERE q.id = c.quote_id AND c.active = false");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
