be19337c519d1d182ace624e46ba5fc1ca10f704
Ryan Maffit <rmaffit@sancsoft.com>
2024-08-16 18:04:34 -0400

    Merge pull request #337 from sancsoft/318-add-additional-statuses
    Added additional statuses

--- src/angular/hq/src/app/enums/staff-status.ts
+++ src/angular/hq/src/app/enums/staff-status.ts
@@ -1,7 +1,9 @@
 export enum StaffStatus {
   InTheOffice = 1,
   WorkingFromHome = 2,
-  Sick = 3,
-  Vacation = 4,
-  Holiday = 5,
+  WorkingFromHomeMorning = 3,
+  WorkingFromHomeAfternoon = 4,
+  Sick = 5,
+  Vacation = 6,
+  Holiday = 7,
 }
