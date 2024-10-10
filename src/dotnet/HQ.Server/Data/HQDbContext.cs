828b504d2e36ac7da3f6a9bdfc7b53026ceea125
amahdysancsoft <amahdy@sancsoft.com>
2024-10-10 10:41:56 -0400

    Added StaffId and Date as a composite index in Points Entity

--- src/dotnet/HQ.Server/Data/HQDbContext.cs
+++ src/dotnet/HQ.Server/Data/HQDbContext.cs
@@ -41,6 +41,7 @@ namespace HQ.Server.Data
             modelBuilder.Entity<Time>().HasIndex(t => t.Date).HasDatabaseName("idx_time_date");
             modelBuilder.Entity<Time>().HasIndex(t => new { t.ChargeCodeId, t.Hours, t.HoursApproved }).HasDatabaseName("idx_time_chargecodeid_hours_hoursapproved");
             modelBuilder.Entity<ProjectStatusReport>().HasIndex(p => new { p.ProjectId, p.StartDate, p.EndDate }).HasDatabaseName("idx_psr_projectid_startdate_enddate");
+            modelBuilder.Entity<Point>().HasIndex(p => new { p.StaffId, p.Date }).HasDatabaseName("idx_point_staffId_date");
             base.OnModelCreating(modelBuilder);
         }
     }
---
+++ src/dotnet/HQ.Server/Data/Migrations/20241010142333_AddIndexPointStaffIdDate.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HQ.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexPointStaffIdDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "idx_point_staffId_date",
+                table: "points",
+                columns: new[] { "staff_id", "date" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "idx_point_staffId_date",
+                table: "points");
+        }
+    }
+}
