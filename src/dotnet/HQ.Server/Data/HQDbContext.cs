14febdaefe3b84214b0df76b9f2e36bdeeea85a4
amrmahdyy <amrmahdy2@gmail.com>
2024-10-11 11:04:39 -0400

    Added index on clientId and ProjectManagerId in project entity

--- src/dotnet/HQ.Server/Data/HQDbContext.cs
+++ src/dotnet/HQ.Server/Data/HQDbContext.cs
@@ -41,6 +41,8 @@ namespace HQ.Server.Data
             modelBuilder.Entity<Time>().HasIndex(t => t.Date).HasDatabaseName("idx_time_date");
             modelBuilder.Entity<Time>().HasIndex(t => new { t.ChargeCodeId, t.Hours, t.HoursApproved }).HasDatabaseName("idx_time_chargecodeid_hours_hoursapproved");
             modelBuilder.Entity<ProjectStatusReport>().HasIndex(p => new { p.ProjectId, p.StartDate, p.EndDate }).HasDatabaseName("idx_psr_projectid_startdate_enddate");
+            modelBuilder.Entity<Project>().HasIndex(p => new { p.ClientId, p.ProjectManagerId }).HasDatabaseName("idx_project_clientid_projectmanagerid");
+
             base.OnModelCreating(modelBuilder);
         }
     }
