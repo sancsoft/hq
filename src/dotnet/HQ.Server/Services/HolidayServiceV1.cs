b1397d04ff2740406c91cdcf31a6a44fea9cabb2
amrmahdyy <amrmahdy2@gmail.com>
2025-01-10 14:34:23 -0500

    Refactored Time entry generation and updated support for previously added holidays

--- src/dotnet/HQ.Server/Services/HolidayServiceV1.cs
+++ src/dotnet/HQ.Server/Services/HolidayServiceV1.cs
@@ -31,33 +31,53 @@ public class HolidayServiceV1
 
     public async Task<Result<UpsertHolidayV1.Response>> UpsertHolidayV1(UpsertHolidayV1.Request request, CancellationToken ct = default)
     {
-        var validationResult = Result.Merge(
+        using (var transaction = await _context.Database.BeginTransactionAsync(System.Data.IsolationLevel.Serializable, ct))
+        {
+
+            try
+            {
+                var validationResult = Result.Merge(
             Result.FailIf(string.IsNullOrEmpty(request.Name), "Name is required."),
             Result.FailIf(await _context.Holidays.AnyAsync(t => t.Id != request.Id && t.Date == request.Date && t.Jurisdiciton == request.Jurisdiciton, ct), "Another holiday already exists on that date.")
         );
 
-        if (validationResult.IsFailed)
-        {
-            return validationResult;
+                if (validationResult.IsFailed)
+                {
+                    return validationResult;
+                }
+
+                var holiday = await _context.Holidays.FindAsync(request.Id);
+                if (holiday == null)
+                {
+                    holiday = new();
+                    _context.Holidays.Add(holiday);
+                }
+
+                holiday.Name = request.Name;
+                holiday.Jurisdiciton = request.Jurisdiciton;
+                holiday.Date = request.Date;
+
+                await _context.SaveChangesAsync(ct);
+
+                var holidayChargeCode = await _context.ChargeCodes.Where(t => t.Project!.Name.ToLower().Contains("holiday")).FirstOrDefaultAsync(ct);
+                if (holidayChargeCode == null)
+                {
+                    return Result.Fail<UpsertHolidayV1.Response>("Holiday charge code not found");
+                }
+                await GenerateTimeEntriesForHoliday(holiday, holidayChargeCode, ct);
+                await transaction.CommitAsync(ct);
+
+                return new UpsertHolidayV1.Response()
+                {
+                    Id = holiday.Id
+                };
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync(ct);
+                return Result.Fail(new Error("An error occurred while upserting the Chargecode.").CausedBy(ex));
+            }
         }
-
-        var holiday = await _context.Holidays.FindAsync(request.Id);
-        if (holiday == null)
-        {
-            holiday = new();
-            _context.Holidays.Add(holiday);
-        }
-
-        holiday.Name = request.Name;
-        holiday.Jurisdiciton = request.Jurisdiciton;
-        holiday.Date = request.Date;
-
-        await _context.SaveChangesAsync(ct);
-
-        return new UpsertHolidayV1.Response()
-        {
-            Id = holiday.Id
-        };
     }
 
     public async Task<Result<DeleteHolidayV1.Response?>> DeleteHolidayV1(DeleteHolidayV1.Request request, CancellationToken ct = default)
@@ -210,6 +230,44 @@ public class HolidayServiceV1
             Updated = updated
         };
     }
+
+    private async Task GenerateTimeEntriesForHoliday(Holiday holiday, ChargeCode holidayChargeCode, CancellationToken ct)
+    {
+        var jurisdiciton = holiday.Jurisdiciton;
+        var staff = _context.Staff.
+        AsNoTracking()
+        .AsQueryable();
+
+        staff = staff.Where(t => t.EndDate == null && t.Jurisdiciton == jurisdiciton);
+        var times = _context.Times
+        .AsNoTracking()
+        .AsQueryable()
+        .Include(t => t.Staff);
+        var staffWithHoliday = await times.Where(t => t.Date == holiday.Date && t.ChargeCode.Id == holidayChargeCode.Id && t.Staff.Jurisdiciton == jurisdiciton && t.Staff.EndDate == null).AsNoTracking().Select(t => t.Staff).ToListAsync(ct);
+
+        var staffWithHolidayIds = staffWithHoliday.Select(s => s.Id).ToList();
+        var staffWithoutEnteredHoliday = await staff.Where(t => !staffWithHolidayIds.Contains(t.Id)).ToListAsync(ct);
+
+        _logger.LogInformation($"Creating time entries for Holiday {holiday.Name} for staff count {staffWithoutEnteredHoliday.Count()}");
+        foreach (var staffMember in staffWithoutEnteredHoliday)
+        {
+            var timeEntry = new Time
+            {
+                ChargeCodeId = holidayChargeCode.Id,
+                Hours = 8,
+                HoursApproved = 8,
+                Notes = holiday.Name,
+                StaffId = staffMember.Id,
+                Date = holiday.Date,
+                Status = TimeStatus.Accepted,
+                HolidayId = holiday.Id
+            };
+            _context.Times.Add(timeEntry);
+        }
+        await _context.SaveChangesAsync(ct);
+        _logger.LogInformation($"Created time entries for Holiday {holiday.Name} for staff count {staffWithoutEnteredHoliday.Count()}");
+    }
+
     public async Task BackgroundAutoGenerateHolidayTimeEntryV1(CancellationToken ct)
     {
         var today = DateOnly.FromDateTime(DateTime.UtcNow);
@@ -219,57 +277,17 @@ public class HolidayServiceV1
         var holidays = _context.Holidays
         .AsNoTracking()
         .AsQueryable();
-        var jurisdicitons = Enum.GetValues(typeof(Jurisdiciton));
         var holidayChargeCode = await _context.ChargeCodes.Where(t => t.Project!.Name.ToLower().Contains("holiday")).FirstOrDefaultAsync(ct);
         if (holidayChargeCode == null)
         {
             return;
         }
-        foreach (Jurisdiciton jurisdiciton in jurisdicitons)
+
+        var upcomingHolidays = await holidays.Where(t => t.Date >= startDate && t.Date <= endDate).ToListAsync(ct);
+        foreach (var upcomingHoliday in upcomingHolidays)
         {
-            var upcomingHolidays = await holidays.Where(t => t.Date >= startDate && t.Date <= endDate && t.Jurisdiciton == jurisdiciton).ToListAsync(ct);
-            foreach (var upcomingHoliday in upcomingHolidays)
-            {
-                var staff = _context.Staff.
-                AsNoTracking()
-                .AsQueryable();
 
-
-                staff = staff.Where(t => t.EndDate == null && t.Jurisdiciton == jurisdiciton);
-                var times = _context.Times
-                .AsNoTracking()
-                .AsQueryable()
-                .Include(t => t.Staff);
-                var staffWithHoliday = await times.Where(t => t.Date == upcomingHoliday.Date && t.ChargeCode.Id == holidayChargeCode.Id && t.Staff.Jurisdiciton == jurisdiciton && t.Staff.EndDate == null).AsNoTracking().Select(t => t.Staff).ToListAsync(ct);
-
-                var staffWithHolidayIds = staffWithHoliday.Select(s => s.Id).ToList();
-                var staffWithoutEnteredHoliday = await staff.Where(t => !staffWithHolidayIds.Contains(t.Id)).ToListAsync(ct);
-
-                _logger.LogInformation($"Creating time entries for Holiday {upcomingHoliday.Name} for staff count {staffWithoutEnteredHoliday.Count()}");
-                foreach (var staffMember in staffWithoutEnteredHoliday)
-                {
-
-                    var timeEntry = new Time
-                    {
-                        ChargeCodeId = holidayChargeCode.Id,
-                        Hours = 8,
-                        HoursApproved = 8,
-                        Notes = upcomingHoliday.Name,
-                        StaffId = staffMember.Id,
-                        Date = upcomingHoliday.Date,
-                        Status = TimeStatus.Accepted,
-                        HolidayId = upcomingHoliday.Id
-                    };
-                    _context.Times.Add(timeEntry);
-                }
-                await _context.SaveChangesAsync(ct);
-
-
-                _logger.LogInformation($"Created time entries for Holiday {upcomingHoliday.Name} for staff count {staffWithoutEnteredHoliday.Count()}");
-            }
-
+            await GenerateTimeEntriesForHoliday(upcomingHoliday, holidayChargeCode, ct);
         }
-
     }
-
 }
