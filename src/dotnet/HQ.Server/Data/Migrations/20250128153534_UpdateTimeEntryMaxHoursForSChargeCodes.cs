e21a61a00006a744dbdd1c13cdfdf1c0d3221dad
amrmahdyy <amrmahdy2@gmail.com>
2025-01-28 11:37:12 -0500

    Update TIme Entry Max Hours for Projects that starts with S

---
+++ src/dotnet/HQ.Server/Data/Migrations/20250128153534_UpdateTimeEntryMaxHoursForSChargeCodes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HQ.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTimeEntryMaxHoursForSChargeCodes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            UPDATE ""projects"" AS p
+            SET ""time_entry_max_hours"" = 24
+            FROM ""charge_codes"" AS c
+            WHERE p.""id"" = c.""project_id""
+            AND c.""code"" LIKE 'S%';
+        ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
