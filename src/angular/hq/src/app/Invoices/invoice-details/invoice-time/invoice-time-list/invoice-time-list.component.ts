f4e0301627d0627baf5732c360f47f3f166a1b2e
Nick Cross <ncross@sancsoft.com>
2026-09-14 11:32:13 -0400

    Dev Build 0.6.3 - Invoicing Feature Test Outcomes 1/ trello card changes.  https://trello.com/c/ICXAeXC1/84-dev-build-063-invoicing-feature-test-outcomes-1

--- src/angular/hq/src/app/Invoices/invoice-details/invoice-time/invoice-time-list/invoice-time-list.component.ts
+++ src/angular/hq/src/app/Invoices/invoice-details/invoice-time/invoice-time-list/invoice-time-list.component.ts
@@ -103,17 +103,39 @@ export class InvoiceTimeListComponent implements OnDestroy {
   }
 
   async updateInvoicedHours(time: GetTimeRecordV1, event: Event) {
+    const inputInvoicedHours = event.target as HTMLInputElement;
     const invoicedHours = (event.target as HTMLInputElement).value;
-    const roundedInvoicedHours = roundToNextQuarter(invoicedHours);
+    let roundedInvoicedHours = roundToNextQuarter(invoicedHours);
+    const numInvoicedHours = parseFloat(invoicedHours);
 
-    if (!time || invoicedHours == '') {
+    if (!time || invoicedHours == '' || numInvoicedHours < 0) {
       await firstValueFrom(
         this.modalService.alert(
           'Error',
           'Please Add a time to your invoiced hours',
         ),
       );
-      return;
+
+      inputInvoicedHours.value = '0';
+      roundedInvoicedHours = 0;
+
+      inputInvoicedHours.classList.remove('border-black');
+      inputInvoicedHours.classList.add(
+        'rounded-none',
+        'border-red-700',
+        'focus:border-red-700',
+        'focus:outline-none',
+      );
+
+      inputInvoicedHours.select();
+    } else {
+      inputInvoicedHours.classList.add('border-black');
+      inputInvoicedHours.classList.remove(
+        'rounded-none',
+        'border-red-700',
+        'focus:border-red-700',
+        'focus:outline-none',
+      );
     }
 
     const request = {
@@ -153,11 +175,11 @@ export class InvoiceTimeListComponent implements OnDestroy {
 
       if (event.id) {
         this.toastService.show('Success', 'Time entry successfully updated.');
-        this.invoiceDetailsService.refresh();
+        this.invoiceDetailsService.invoiceRefresh();
         // this.planningPointsRequestTrigger$.next(); // TODO: Trigger this
       } else {
         this.toastService.show('Success', 'Time entry successfully created.');
-        this.invoiceDetailsService.refresh();
+        this.invoiceDetailsService.invoiceRefresh();
         // this.planningPointsRequestTrigger$.next();
       }
     } catch (err) {
